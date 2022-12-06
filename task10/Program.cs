cfa37c57b2aa36c03b5eafecff104c2f68e5dc8a
Natalia <dginni2009@gmail.com>
2022-12-06 15:21:10 +0300

    commit2

---
+++ task10/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Enter a three-digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int GetSecondDigit(int number)
+{
+    int secondDigit = number / 10 % 10;
+    return secondDigit;
+} 
+
+if ((number > 99) && (number < 1000))
+{
+    Console.WriteLine($"The second digit of the number is {GetSecondDigit(number)}");
+} else
+{
+    Console.WriteLine("Enter another number");
+}
---
+++ task13/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+string numberStr = Convert.ToString(number);
+if (numberStr.Length > 2)
+{
+    Console.WriteLine($"Third number is {numberStr[2]}");
+} else 
+{
+    Console.WriteLine("No third number");
+}
+
+// не знаю, как решить это через математику, ведь неизвестно, какое число будет вводится
